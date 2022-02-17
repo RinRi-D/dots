0d86504fd7ae8bcc228532f7fc4a6f678e68f26d
RinRi <rin@rinri-d.xyz>
2022-02-17 16:48:36 +0300

    xinit.sh

--- scripts/xinit.sh
+++ scripts/xinit.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+lxpolkit &
 slstatus &
 dunst &
 setxkbmap -layout us,ru -option grp:alt_shift_toggle &
 xbanish &
 xrdb ~/.Xresources &
-feh --bg-scale ~/walls/collections/wallhaven-wyrqg7.png &
+wmname LG3D &
+feh --bg-fill ~/walls/current.png &
 picom --experimental-backends &
 xautolock -time 5 -locker slock.sh &
 brightnessctl set 100% &
+wmname LG3D &
+emacs --daemon &
+eval $(gnome-keyring-daemon --start) &
+export SSH_AUTH_SOCK &
+#killall pulseaudio
+~/scripts/start-jack.sh &
+cadence &
+mount ~/nextcloud &
